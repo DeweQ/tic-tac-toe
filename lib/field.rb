e5266b6c2b4b9c70d81d48e861d3bbfd999f04d5
DeweQ <thedeweq@yandex.ru>
2025-08-18 23:52:17 +0300

    Add finished? and check_winner to Field

--- lib/field.rb
+++ lib/field.rb
@@ -15,6 +15,25 @@ class Field
     @field[y_cord][x_cord] = mark if accessible?(x_cord, y_cord)
   end
 
+  def finished?
+    @field.all? do |row|
+      row.all? { |cell| cell != " " }
+    end
+  end
+
+  def check_winner
+    return unless finished?
+
+    winner = "Draw"
+    @field.any? { |row| winner = row[0] if row.uniq.size == 1 }
+    @field.transpose.any? { |column| winner = column[0] if column.uniq.size == 1 }
+    diagonal = (0...@size).map { |i| @field[i][i] }
+    winner = diagonal[0] if diagonal.uniq.size == 1
+    anti_diagonal = (0...@size).map { |i| @field[i][@size - 1 - i] }
+    winner = anti_diagonal[0] if anti_diagonal.uniq.size == 1
+    winner
+  end
+
   def to_s
     table = Terminal::Table.new headings: [0, 1, 2, "x/y"], rows: @field, style: { all_separators: true }
     (0...@size).each { |i| table.rows[i] << i }
