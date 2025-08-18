812117afa2d101749aabd4dad4bed76c8efd5e52
DeweQ <thedeweq@yandex.ru>
2025-08-18 16:00:13 +0300

    Add ability to make a mark in field
    Also change the way accessible? is working (conditionals -> dig)
    Add coordinates to to_s output

--- lib/field.rb
+++ lib/field.rb
@@ -4,20 +4,21 @@ require "terminal-table"
 class Field
   def initialize(size = 3)
     @size = size
-    @field = Array.new(size) { Array.new(size) }
+    @field = Array.new(size) { Array.new(size) { " " } }
   end
 
   def accessible?(x_cord, y_cord)
-    @field[y_cord][x_cord].nil?
+    @field.dig(y_cord, x_cord) == " "
+  end
+
+  def add_mark(mark, x_cord, y_cord)
+    @field[y_cord][x_cord] = mark if accessible?(x_cord, y_cord)
   end
 
   def to_s
-    Terminal::Table.new do |t|
-      t << @field[0]
-      @field[1..].each do |row|
-        t << :separator
-        t << row
-      end
-    end.to_s
+    table = Terminal::Table.new headings: [0, 1, 2, "x/y"], rows: @field, style: { all_separators: true }
+    (0...@size).each { |i| table.rows[i] << i }
+    table.align_column(3, :center)
+    table.to_s
   end
 end
