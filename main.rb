71832188907e8b4668706e9313e8f1797eace0a1
DeweQ <thedeweq@yandex.ru>
2025-08-19 13:45:13 +0300

    Set up a game loop in main.rb

---
+++ main.rb
@@ -0,0 +1,22 @@
+require_relative "lib/tic_tac_toe"
+
+loop do
+  puts "=" * 80
+  puts "NEW GAME"
+  game = TicTacToe.new
+  until game.finished?
+    puts game.current_player == "X" ? "It's Crosses turn: " : "It's Noughts turn"
+    game.print_field
+    puts "Enter coordinates separated by space (ex. '0 0')"
+    coords = gets.split.map(&:to_i)
+    puts "Invalid coordinates" if game.mark_field(coords[0], coords[1]) == "Invalid coordinates"
+    puts "*" * 80
+  end
+  winner = game.check_winner
+  if winner == "None"
+    puts "Game ended in a draw!"
+  else
+    puts "Game finished! #{game.check_winner == 'X' ? 'Crosses' : 'Noughts'} wins!"
+  end
+  puts "Press Ctrl+C to exits"
+end
