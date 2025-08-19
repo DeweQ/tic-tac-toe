2309a8fef149ad9a4637d9e154875430bcd965ea
DeweQ <thedeweq@yandex.ru>
2025-08-19 12:31:31 +0300

    Add Enumerable and index accessor to field
    Also extracted finished? and check_winner from class to later add them
    to TicTacToe

--- lib/field.rb
+++ lib/field.rb
@@ -2,9 +2,53 @@ require "terminal-table"
 
 # Class containing a field for Tic Tac Toe game
 class Field
+  include Enumerable
+
+  # Protected row class to grant access to it only for Field class
+  class ProtectedRow
+    include Enumerable
+
+    def initialize(size = 3)
+      @size = size
+      @row = Array.new(size) { " " }
+    end
+
+    def each(&block)
+      row.each(&block)
+    end
+
+    def dig(*keys)
+      row.dig(*keys)
+    end
+
+    def [](index)
+      row[index]
+    end
+
+    def []=(index, value)
+      row[index] = value
+    end
+
+    def to_s
+      row.to_s
+    end
+
+    protected
+
+    attr_accessor :row
+  end
+
   def initialize(size = 3)
     @size = size
-    @field = Array.new(size) { Array.new(size) { " " } }
+    @field = Array.new(size) { ProtectedRow.new(@size) }
+  end
+
+  def each(&block)
+    field.each(&block)
+  end
+
+  def [](index)
+    field[index]
   end
 
   def accessible?(x_cord, y_cord)
@@ -15,29 +59,14 @@ class Field
     @field[y_cord][x_cord] = mark if accessible?(x_cord, y_cord)
   end
 
-  def finished?
-    @field.all? do |row|
-      row.all? { |cell| cell != " " }
-    end
-  end
-
-  def check_winner
-    return unless finished?
-
-    winner = "Draw"
-    @field.any? { |row| winner = row[0] if row.uniq.size == 1 }
-    @field.transpose.any? { |column| winner = column[0] if column.uniq.size == 1 }
-    diagonal = (0...@size).map { |i| @field[i][i] }
-    winner = diagonal[0] if diagonal.uniq.size == 1
-    anti_diagonal = (0...@size).map { |i| @field[i][@size - 1 - i] }
-    winner = anti_diagonal[0] if anti_diagonal.uniq.size == 1
-    winner
-  end
-
   def to_s
     table = Terminal::Table.new headings: [0, 1, 2, "x/y"], rows: @field, style: { all_separators: true }
     (0...@size).each { |i| table.rows[i] << i }
     table.align_column(3, :center)
     table.to_s
   end
+
+  protected
+
+  attr_accessor :field
 end
