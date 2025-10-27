8124e39faf7d21132854a49f5bd0e5edfc934c27
DeweQ <thedeweq@yandex.ru>
2025-10-27 23:51:07 +0300

    Add tests for #check_winner

--- spec/tic-tac-toe_spec.rb
+++ spec/tic-tac-toe_spec.rb
@@ -14,7 +14,8 @@ describe TicTacToe do
     let(:field_class) {class_double("Field")}
     let(:unaccessible_coord) {-1}
     let(:field) { instance_double("Field") }
-        subject(:stubbed_game) { described_class.new}
+    subject(:stubbed_game) { described_class.new}
+
     context "when given unaccessible coordinates" do
       before do
         stub_const("Field", field_class)
@@ -60,6 +61,53 @@ describe TicTacToe do
     end
   end
 
-  describe "#check_winner" do end
+  describe "#check_winner" do 
+    context "when game has no winner" do
+    subject(:nowinner_game) {described_class.new}
+      it "returns nil" do
+        expect(nowinner_game.check_winner).to be_nil
+      end
+    end
+    
+    context "when game has winner in row" do
+      subject(:row_game) {described_class.new}
+      before do
+        field = row_game.instance_variable_get(:@field)
+        field.add_mark("X",0,0)
+        field.add_mark("X",0,1)
+        field.add_mark("X",0,2)
+      end
+      it "returns winner's mark" do
+        expect(row_game.check_winner).to eq("X")
+      end
+    end
+
+    context "when game has winner in column" do
+      subject(:column_game) {described_class.new}
+      before do
+        field = column_game.instance_variable_get(:@field)
+        field.add_mark("O",0,1)
+        field.add_mark("O",1,1)
+        field.add_mark("O",2,1)
+      end
+      it "returns winner's mark" do
+        expect(column_game.check_winner).to eq("O")
+      end
+    end
+
+    context "when game has winner in diagonal" do
+      subject(:diagonal_game) {described_class.new}
+      before do
+        field = diagonal_game.instance_variable_get(:@field)
+        field.add_mark("X",0,0)
+        field.add_mark("X",1,1)
+        field.add_mark("X",2,2)
+      end
+      it "returns winner's mark" do
+        expect(diagonal_game.check_winner).to eq("X")
+      end
+    end
+  end
+
   describe "#finished?" do end
 end
